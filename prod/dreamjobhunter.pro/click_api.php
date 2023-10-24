bcd2c1a53d7c60f36bd3ee9b28b0d24f4bfe987b
Pavlo Svystun <nefely123@gmail.com>
2023-10-24 20:53:17 +0300

    done

--- prod/dreamjobhunter.pro/click_api.php
+++ prod/dreamjobhunter.pro/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='voz9c4q4eizel9cezan5';	
+    $CampaignLink='https://track.dreamjobhunter.pro/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
