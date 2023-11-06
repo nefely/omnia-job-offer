eb4861f1dfce51a8063b45c888aa481b83698dbf
Pavlo Svystun <nefely123@gmail.com>
2023-11-06 14:17:13 +0200

    domain backup

---
+++ prod/dreamjobfinder.pro-old/amazon-product-tester/click.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/javascript');
+$clickid = "optional";
+if(isset($_GET['key']) || isset($_GET['uclick'])) {
+    $url = "https://track.dreamjobfinder.pro/track.php?{$_SERVER['QUERY_STRING']}";
+    $headers = get_headers($url, 1);
+    $domain = parse_url(is_array($headers['Location'])?$headers['Location'][0]:$headers['Location'], PHP_URL_HOST);
+    if($domain != null) {
+        parse_url(is_array($headers['Location'])?$headers['Location'][0]:$headers['Location'], PHP_URL_HOST);
+        $query_str = parse_url(is_array($headers['Location'])?$headers['Location'][0]:$headers['Location'], PHP_URL_QUERY);
+        parse_str($query_str, $query_params);
+        $response = array('domain' => $domain) + $query_params;
+        $clickid = isset($response['clickid']) ? $response['clickid'] : $response['s2'];
+    }
+}
+print_r("var clickid = '{$clickid}'");
+die();
