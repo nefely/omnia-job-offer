61a18a1a47f98ab153caf217e520571c00455311
Pavlo Svystun <nefely123@gmail.com>
2023-11-06 15:38:09 +0200

    done

--- prod/thejobfinder.net/amazon-product-tester-ca/click_api.php
+++ prod/thejobfinder.net/amazon-product-tester-ca/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
     $CampaignKey='tdtzjgnojavi66lz7g0d';	
-    $CampaignLink='https://track.dreamjobfinder.pro/track.php?key='.$CampaignKey;
+    $CampaignLink='https://track.thejobfinder.net/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
