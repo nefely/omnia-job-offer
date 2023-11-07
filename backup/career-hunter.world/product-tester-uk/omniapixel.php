ed10887f960b118582c0c8d4e2ba4a6447468f2e
Pavlo Svystun <nefely123@gmail.com>
2023-11-07 22:24:36 +0200

    fuck

---
+++ backup/career-hunter.world/product-tester-uk/omniapixel.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/javascript');
+
+$CampaignKey = 'eu55s9x513j14qby9xzs';
+$url = "https://track.career-hunter.world/track.php?key=".$CampaignKey;
+$headers = @get_headers($url, 1);
+
+$domain = @parse_url(@$headers['Location'], PHP_URL_HOST);
+$data = new ArrayObject();
+
+if($domain != null && isset($headers['Location'])) {
+    @parse_url(@$headers['Location'], PHP_URL_HOST);
+    $query_str = @parse_url($headers['Location'], PHP_URL_QUERY);
+    @parse_str($query_str, $query_params);
+	$data = json_encode($query_params);
+}
+
+@$omniaPixelData = json_encode($data);
+
+print_r("console.log('OmniPixel: {$omniaPixelData}')");
+die();
