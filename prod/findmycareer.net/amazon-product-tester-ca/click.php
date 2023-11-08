a06b1f170f17877af942c644383d4599fec330f6
Pavlo Svystun <nefely123@gmail.com>
2023-11-08 13:22:09 +0200

    done

--- prod/findmycareer.net/amazon-product-tester-ca/click.php
+++ prod/findmycareer.net/amazon-product-tester-ca/click.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/javascript');
 $clickid = "optional";
 if(isset($_GET['key']) || isset($_GET['uclick'])) {
-    $url = "https://track.cloackandtrackit.com/track.php?{$_SERVER['QUERY_STRING']}";
+    $url = "https://track.findmycareer.net/track.php?{$_SERVER['QUERY_STRING']}";
     $headers = get_headers($url, 1);
     $domain = parse_url(is_array($headers['Location'])?$headers['Location'][0]:$headers['Location'], PHP_URL_HOST);
     if($domain != null) {
