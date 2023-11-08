3d539ca604bc078606eb35bf77b623ae2b82600e
Pavlo Svystun <nefely123@gmail.com>
2023-11-08 18:48:18 +0200

    prod done

--- prod/work-hunter.pro/apple-product-tester-usa/click_api.php
+++ prod/work-hunter.pro/apple-product-tester-usa/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='kdx2p360kob5xas82bwo';	
+    $CampaignLink='https://track.work-hunter.pro/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
