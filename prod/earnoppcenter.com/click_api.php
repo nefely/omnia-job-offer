3122b31f5cbc9b766275c74855249b1c93132a68
Pavlo Svystun <nefely123@gmail.com>
2023-11-15 17:54:22 +0200

    done

--- prod/earnoppcenter.com/click_api.php
+++ prod/earnoppcenter.com/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='ovfgx0mv3vhdfvmakzi8';	
+    $CampaignLink='https://track.earnoppcenter.com/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
