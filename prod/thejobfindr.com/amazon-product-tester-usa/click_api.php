b955a9aab133faf6f7569124baccb557c742bcd3
Pavlo Svystun <nefely123@gmail.com>
2023-11-16 22:40:03 +0200

    done

--- prod/thejobfindr.com/amazon-product-tester-usa/click_api.php
+++ prod/thejobfindr.com/amazon-product-tester-usa/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='yej54irjkm1iih8ty9ao';	
+    $CampaignLink='https://track.thejobfindr.com/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
