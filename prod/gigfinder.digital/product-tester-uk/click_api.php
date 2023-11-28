997cc0ee91394e69a643f9152205d915e913e921
Pavlo Svystun <nefely123@gmail.com>
2023-11-28 17:31:32 +0200

    done prod

--- prod/gigfinder.digital/product-tester-uk/click_api.php
+++ prod/gigfinder.digital/product-tester-uk/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='lg84c0msxebdcyftifh6';	
+    $CampaignLink='https://track.gigfinder.digital/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
