6f2d8e54d48b533f6c33e9e0b798047afc49f35f
Pavlo Svystun <nefely123@gmail.com>
2023-11-30 18:41:46 +0200

    done

--- prod/startmycareer.pro/product-tester-ca/click_api.php
+++ prod/startmycareer.pro/product-tester-ca/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
     $CampaignKey='so6lkzgk58qx2hobo1im';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignLink='https://track.startmycareer.pro/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
