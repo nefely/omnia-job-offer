25eea2ce8c24b393b5ea43c329212aac8bb5f0a7
Pavlo Svystun <nefely123@gmail.com>
2023-12-08 14:44:34 +0200

    done

--- template/16-Product-Tester-UK-NOFORM/product-tester-uk-noform/js/script_main.js
+++ template/16-Product-Tester-UK-NOFORM/product-tester-uk-noform/js/script_main.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
 
-if ($("input[name=phone]").length > 0) {
-  $("input[name=phone]").mask('00000000000');
-}
-$("input[name=firstname]").val("")
-$("input[name=lastname]").val("")
 $("input[name=email]").val("")
-$("input[name=phone]").val("")
 
 $(".subscribe button").click(function(){
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($(".subscribe input").val())) {
@@ -65,38 +59,6 @@ $(".question--1 .submit button").click(function(){
 
 
 // question 2
-isFirstNameValid = () => {
-  if ($("input[name=firstname]").val().length >= 2) {
-    return true
-  } else {
-    return false
-  }
-}
-firstNameValidation = () => {
-  if (isFirstNameValid()) {
-    $("input[name=firstname]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=firstname]").focus()
-    $("input[name=firstname]").closest(".field-content").addClass("error")
-  }
-}
-
-isLastNameValid = () => {
-  if ($("input[name=lastname]").val().length >= 2) {
-    return true
-  } else {
-    return false
-  }
-}
-lastNameValidation = () => {
-  if (isLastNameValid()) {
-    $("input[name=lastname]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=lastname]").focus()
-    $("input[name=lastname]").closest(".field-content").addClass("error")
-  }
-}
-
 isEmailValid = () => {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
     return true
@@ -115,69 +77,21 @@ emailValidation = () => {
 
 $(".question--2 .submit").click(function(){
   emailValidation()
-  lastNameValidation()
-  firstNameValidation()
 });
 
-$(".question--2 .submit button").click(function(){
-  $(this).closest(".question").fadeOut(0).next(".question").fadeIn(0)
-});
+$(".question--2 .submit button").click(function(){});
 
 $(".question--2 input").on("input" , function(){
-  if (isFirstNameValid() && isLastNameValid() && isEmailValid()) {
+  if (isEmailValid()) {
     $(this).closest(".question").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
   } else {
     $(this).closest(".question").find(".submit button").css("pointer-events" , "none").addClass("disabled");
   }
 })
 
-$("input[name=firstname]").on("input" , function(){
-  if ($(this).val().length >= 2) {
-    $(this).closest(".field-content").removeClass("error")
-  }
-})
-$("input[name=lastname]").on("input" , function(){
-  if ($(this).val().length >= 2) {
-    $(this).closest(".field-content").removeClass("error")
-  }
-})
 $("input[name=email]").on("input" , function(){
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
     $("input[name=email]").closest(".field-content").removeClass("error")
-  }
-})
-
-// question 3
-isPhoneValid = () => {
-  if ($("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 10 || $("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 11) {
-    return true
-  } else {
-    return false
-  }
-}
-phoneValidation = () => {
-  if (isPhoneValid()) {
-    $("input[name=phone]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=phone]").focus()
-    $("input[name=phone]").closest(".field-content").addClass("error")
-  }
-}
-
-$(".question--3 .submit").click(function(){
-  phoneValidation()
-});
-
-$(".question--3 input").on("input" , function(){
-  if (isPhoneValid()) {
-    $(this).closest(".question").find(".submit a").css("pointer-events" , "initial").removeClass("disabled");
-  } else {
-    $(this).closest(".question").find(".submit a").css("pointer-events" , "none").addClass("disabled");
-  }
-})
-$("input[name=phone]").on("input" , function(){
-  if ($("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 10 || $("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 11) {
-    $(this).closest(".field-content").removeClass("error")
   }
 })
 
@@ -195,10 +109,10 @@ $(".offer-link").click(function(e){
 
   const data = {
     "zip": "", 
-    "firstname": $("[name=firstname]").val(), 
-    "lastname": $("[name=lastname]").val(), 
+    "firstname": "", 
+    "lastname": "", 
     "email": $("[name=email]").val(), 
-    "phone": $("[name=phone]").val().replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "").replaceAll("-", "").replaceAll("+", ""), 
+    "phone": "", 
     "offer_type": $("[name=offer_type]").val(), 
     "offer_url": window.location.href.split('?')[0], 
     "click_id": clickid
@@ -215,7 +129,7 @@ $(".offer-link").click(function(e){
           
           setTimeout(()=>{
               $(".offer-link").css("display","block").css("visibility","visible")
-              window.location.href = `survey/?clickid=${clickid}&uclick=${uclick}&zip=${data.zip}&firstname=${data.firstname}&lastname=${data.lastname}&email=${data.email}&telephone=${data.phone.replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "").replaceAll("-", "")}`
+              window.location.href = `survey/?clickid=${clickid}&uclick=${uclick}&email=${data.email}`
           },300)
 
         })
@@ -227,20 +141,13 @@ $(".offer-link").click(function(e){
 
 setTimeout(()=>{
 
-if (isFirstNameValid() && isLastNameValid() && isEmailValid()) {
+if (isEmailValid()) {
   $(".question--2").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
 } else {
   $(".question--2").find(".submit button").css("pointer-events" , "none").addClass("disabled");
 }
 
-if (isPhoneValid()) {
-  $(".question--3").find(".submit a").css("pointer-events" , "initial").removeClass("disabled");
-} else {
-  $(".question--3").find(".submit a").css("pointer-events" , "none").addClass("disabled");
-}
-
-console.log(isFirstNameValid() && isLastNameValid() && isEmailValid())
-console.log(isPhoneValid())
+console.log(isEmailValid())
 
 }, 1500)
 
