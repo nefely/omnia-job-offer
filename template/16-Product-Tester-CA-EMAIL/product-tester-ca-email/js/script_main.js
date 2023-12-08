187664c60583e2dfe3959ebb6cb1b21543f03d36
Pavlo Svystun <nefely123@gmail.com>
2023-12-08 15:12:34 +0200

    done

--- template/16-Product-Tester-CA-EMAIL/product-tester-ca-email/js/script_main.js
+++ template/16-Product-Tester-CA-EMAIL/product-tester-ca-email/js/script_main.js
@@ -1,22 +1,6 @@
 $(document).ready(function(){
 
-if ($("input[name=phone]").length > 0) {
-  $("input[name=phone]").mask('(000) 000-0000');
-}
-if ($("input[name=zip]").length > 0) {
-    $("input[name=zip]").mask("A0A 0A0", {
-        'translation': {
-            A: { pattern: /[A-Za-z0-9]/ },
-            0: { pattern: /[A-Za-z0-9]/ }
-        }
-    });
-}
-
-$("input[name=zip]").val("")
-$("input[name=firstname]").val("")
-$("input[name=lastname]").val("")
 $("input[name=email]").val("")
-$("input[name=phone]").val("")
 
 $(".subscribe button").click(function(){
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($(".subscribe input").val())) {
@@ -62,86 +46,19 @@ if ($('.swiper').length > 0) {
 
 
 
-// question 1
-isZipValid = () => {
-  if ($("input[name=zip]").val().length == 7) {
-    return true
-  } else {
-    return false
-  }
-}
-zipValidation = () => {
-  if (isZipValid()) {
-    $("input[name=zip]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=zip]").focus()
-    $("input[name=zip]").closest(".field-content").addClass("error")
-  }
-}
-
-$(".question--1 .submit").click(function(){
-  zipValidation()
-});
+$(".question--1 .submit").click(function(){});
+$(".question--1").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
 
 $(".question--1 .submit button").click(function(){
   if (window.matchMedia("(max-width: 991px)").matches) {
     $(".intro[data-prepop=1]").addClass("bg-non");
   }
-
-
   $(".container-bullets").remove()
   $(this).closest(".question").fadeOut(0).next(".question").fadeIn(0)
 });
 
-$(".question--1 input").on("input" , function(){
-  if (isZipValid()) {
-    $(this).closest(".question").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
-  } else {
-    $(this).closest(".question").find(".submit button").css("pointer-events" , "none").addClass("disabled");
-  }
-})
-
-$("input[name=zip]").on("input" , function(){
-    let currentValue = $(this).val().toUpperCase();
-    $(this).val(currentValue);
-    if ($(this).val().length == 7) {
-        $(this).closest(".field-content").removeClass("error")
-    }
-})
 
 // question 2
-isFirstNameValid = () => {
-  if ($("input[name=firstname]").val().length >= 2) {
-    return true
-  } else {
-    return false
-  }
-}
-firstNameValidation = () => {
-  if (isFirstNameValid()) {
-    $("input[name=firstname]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=firstname]").focus()
-    $("input[name=firstname]").closest(".field-content").addClass("error")
-  }
-}
-
-isLastNameValid = () => {
-  if ($("input[name=lastname]").val().length >= 2) {
-    return true
-  } else {
-    return false
-  }
-}
-lastNameValidation = () => {
-  if (isLastNameValid()) {
-    $("input[name=lastname]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=lastname]").focus()
-    $("input[name=lastname]").closest(".field-content").addClass("error")
-  }
-}
-
 isEmailValid = () => {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
     return true
@@ -160,69 +77,21 @@ emailValidation = () => {
 
 $(".question--2 .submit").click(function(){
   emailValidation()
-  lastNameValidation()
-  firstNameValidation()
 });
 
-$(".question--2 .submit button").click(function(){
-  $(this).closest(".question").fadeOut(0).next(".question").fadeIn(0)
-});
+$(".question--2 .submit button").click(function(){});
 
 $(".question--2 input").on("input" , function(){
-  if (isFirstNameValid() && isLastNameValid() && isEmailValid()) {
+  if (isEmailValid()) {
     $(this).closest(".question").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
   } else {
     $(this).closest(".question").find(".submit button").css("pointer-events" , "none").addClass("disabled");
   }
 })
 
-$("input[name=firstname]").on("input" , function(){
-  if ($(this).val().length >= 2) {
-    $(this).closest(".field-content").removeClass("error")
-  }
-})
-$("input[name=lastname]").on("input" , function(){
-  if ($(this).val().length >= 2) {
-    $(this).closest(".field-content").removeClass("error")
-  }
-})
 $("input[name=email]").on("input" , function(){
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
     $("input[name=email]").closest(".field-content").removeClass("error")
-  }
-})
-
-// question 3
-isPhoneValid = () => {
-  if ($("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 10) {
-    return true
-  } else {
-    return false
-  }
-}
-phoneValidation = () => {
-  if (isPhoneValid()) {
-    $("input[name=phone]").closest(".field-content").removeClass("error")
-  } else {
-    $("input[name=phone]").focus()
-    $("input[name=phone]").closest(".field-content").addClass("error")
-  }
-}
-
-$(".question--3 .submit").click(function(){
-  phoneValidation()
-});
-
-$(".question--3 input").on("input" , function(){
-  if (isPhoneValid()) {
-    $(this).closest(".question").find(".submit a").css("pointer-events" , "initial").removeClass("disabled");
-  } else {
-    $(this).closest(".question").find(".submit a").css("pointer-events" , "none").addClass("disabled");
-  }
-})
-$("input[name=phone]").on("input" , function(){
-  if ($("input[name=phone]").val().replaceAll("(" , "").replaceAll(")" , "").replaceAll("-" , "").replaceAll(" " , "").length == 10) {
-    $(this).closest(".field-content").removeClass("error")
   }
 })
 
@@ -239,11 +108,11 @@ $(".offer-link").click(function(e){
   // const uclick = "my-uclick";
 
   const data = {
-    "zip": $("[name=zip]").val(), 
-    "firstname": $("[name=firstname]").val(), 
-    "lastname": $("[name=lastname]").val(), 
+    "zip": "", 
+    "firstname": "", 
+    "lastname": "", 
     "email": $("[name=email]").val(), 
-    "phone": $("[name=phone]").val(), 
+    "phone": "", 
     "offer_type": $("[name=offer_type]").val(), 
     "offer_url": window.location.href.split('?')[0], 
     "click_id": clickid
@@ -260,7 +129,7 @@ $(".offer-link").click(function(e){
           
           setTimeout(()=>{
               $(".offer-link").css("display","block").css("visibility","visible")
-              window.location.href = `survey/?clickid=${clickid}&uclick=${uclick}&zip=${data.zip}&firstname=${data.firstname}&lastname=${data.lastname}&email=${data.email}&telephone=${data.phone.replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "").replaceAll("-", "")}`
+              window.location.href = `survey/?clickid=${clickid}&uclick=${uclick}&email=${data.email}`
           },300)
 
         })
@@ -272,27 +141,13 @@ $(".offer-link").click(function(e){
 
 setTimeout(()=>{
 
-if (isZipValid()) {
-  $(".question--1").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
-} else {
-  $(".question--1").find(".submit button").css("pointer-events" , "none").addClass("disabled");
-}
-
-if (isFirstNameValid() && isLastNameValid() && isEmailValid()) {
+if (isEmailValid()) {
   $(".question--2").find(".submit button").css("pointer-events" , "initial").removeClass("disabled");
 } else {
   $(".question--2").find(".submit button").css("pointer-events" , "none").addClass("disabled");
 }
 
-if (isPhoneValid()) {
-  $(".question--3").find(".submit a").css("pointer-events" , "initial").removeClass("disabled");
-} else {
-  $(".question--3").find(".submit a").css("pointer-events" , "none").addClass("disabled");
-}
-
-console.log(isZipValid())
-console.log(isFirstNameValid() && isLastNameValid() && isEmailValid())
-console.log(isPhoneValid())
+console.log(isEmailValid())
 
 }, 1500)
 
