f1f8bfb830e1c5101e1b28a7c81053e7f6bd5644
Pavlo Svystun <nefely123@gmail.com>
2023-12-12 15:25:13 +0200

    done

--- prod/work-hunter.com/amazon-product-tester-uk-v2/click_api.php
+++ prod/work-hunter.com/amazon-product-tester-uk-v2/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='9wwsutxqdr3un7a7nolq';	
+    $CampaignLink='https://track.work-hunter.com/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
