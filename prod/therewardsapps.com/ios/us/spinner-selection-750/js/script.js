54032b7508771cecd175ae3129f0eefcc077404e
Pavlo Svystun <nefely123@gmail.com>
2023-12-20 17:48:54 +0200

    click to track

--- prod/therewardsapps.com/ios/us/spinner-selection-750/js/script.js
+++ prod/therewardsapps.com/ios/us/spinner-selection-750/js/script.js
@@ -35,7 +35,7 @@ $(".congratulation .btn-submit").click(function() {
       fetch(`https://track.therewardsapps.com/track.php?cnv_id=${clickid}`, {
 	  mode: 'no-cors'
       })
-      .then(() => { window.location.href = `https://track.therewardsapps.com/click.php?lp=1&email=${email}&to_offer=${offer_number}` })
+      .then(() => { window.location.href = `https://track.therewardsapps.com/track.php?lp=1&email=${email}&to_offer=${offer_number}` })
       .catch((e) => console.log(`Failed to send Binom lead with clickid: ${clickid}. Reason: ${e.message}`));
     }
 });
