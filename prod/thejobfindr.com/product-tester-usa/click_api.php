b3427c2a24a241522bf930611b8ebfcc01439015
Pavlo Svystun <nefely123@gmail.com>
2024-01-04 21:35:02 +0200

    done

--- prod/thejobfindr.com/product-tester-usa/click_api.php
+++ prod/thejobfindr.com/product-tester-usa/click_api.php
@@ -1,7 +1,7 @@
 <?php
 
-    $CampaignKey='7mdm0kvambi6ji0qxw8k';	
-    $CampaignLink='https://track.cloackandtrackit.com/track.php?key='.$CampaignKey;
+    $CampaignKey='v6m6luv8f8aeawt3jvuz';	
+    $CampaignLink='https://track.thejobfindr.com/track.php?key='.$CampaignKey;
     $ApiKey='10000013adee86ee9f777ada9fb323e24fccaf3';
 
     $getClick = new getClick($CampaignLink, $ApiKey);
