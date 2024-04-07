0400fddf778af26ee8a6363f08de1e2827f91d4a
Pavlo Svystun <nefely123@gmail.com>
2024-04-07 18:56:43 +0300

    +

--- template/26-Product-Tester-Amazon-UK/amazon-product-tester-uk/js/script_main.js
+++ template/26-Product-Tester-Amazon-UK/amazon-product-tester-uk/js/script_main.js
@@ -222,7 +222,8 @@ $(".form-step--3 .btn-next").click(function(e){
     };
 
     // uncommit on prod
-    fetch(`https://track.work-hunter.com/postback?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
+    // fetch(`https://track.work-hunter.com/postback?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
+    fetch(`https://track.work-hunter.com/preclick?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
     .then(r => {
         console.log("successfully registered: " + rtkClickID);
         fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
