a148fe3fa31a7e3aea7b0d162ed0f8cd620c22ef
Pavlo Svystun <nefely123@gmail.com>
2024-08-13 13:25:56 +0300

    +

--- template/32-Product-Tester-Levelcash/levelcash-product-tester/js/script.js
+++ template/32-Product-Tester-Levelcash/levelcash-product-tester/js/script.js
@@ -48,36 +48,11 @@ $(document).ready(function(){
 
     $(".btn-submit").click(function(event){
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
-            event.preventDefault();
-
-            $(this).css("display","none").css("visibility","hidden")
-	
-            const data = {
-                "zip": "11111", 
-                "firstname": "test", 
-                "lastname": "test", 
-                "phone": "11111111111",
-                "email": $("[name=email]").val(), 
-                "offer_type": $("[name=offer_type]").val(), 
-                "offer_url": window.location.href.split('?')[0], 
-                "click_id": rtkClickID
-            };
-            
-            fetch(`https://track.oppcenter.net/postback?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
-            .then(r => {
-                console.log("successfully registered: " + rtkClickID);
-                fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
-                .then(rr => {
-                    console.log("successfully registered lead in Data API: " + rtkClickID)
-                    setTimeout(()=>{
-                        $(this).css("display","flex").css("visibility","visible")
-                        window.location.href = $(".btn-submit").attr('href');
-                    }, 300)
-                })
-                .catch(ed => { window.location.href = $(".btn-submit").attr('href') });
-            })
-            .catch(e => console.log("error during registration lead: " + e));
-
+            $(this).css("display","none").css("visibility","didden")
+            window.location.href = $(".btn-submit").attr('href');
+            setTimeout(()=>{
+                $(this).css("display","flex").css("visibility","visible")
+            },200)
         } else {
             event.preventDefault();
             $("input[name=email]").addClass('error')
