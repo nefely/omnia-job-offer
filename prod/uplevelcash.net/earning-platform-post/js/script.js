853aa616e1680527ef7716ec1137c72d3893e6ae
Pavlo Svystun <nefely123@gmail.com>
2024-08-21 20:18:22 +0300

    fuck

--- prod/uplevelcash.net/earning-platform-post/js/script.js
+++ prod/uplevelcash.net/earning-platform-post/js/script.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
     window.domain = window.location.hostname;
     $(".domain").text(domain)
     $(".mailto").attr("href" , `mailto:contact@${domain}`)
+
+    $(window).on('scroll', function() {
+        var footer = $('footer');
+        var button = $('.float-button');
+        
+        var footerTop = footer.offset().top;
+        var windowBottom = $(window).scrollTop() + $(window).height();
+
+        if (windowBottom >= footerTop) {
+            button.addClass('atbottom'); 
+        } else {
+            button.removeClass('atbottom');
+        }
+    });
+    
 })
