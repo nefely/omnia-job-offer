e33b21d4c517f990fbe1212c0abf7bdd604ab99f
Pavlo Svystun <nefely123@gmail.com>
2024-08-22 18:37:29 +0300

    new template 33

--- template/33-Product-Reviewer-Amazon-USA/amazon-product-reviewer-usa/js/script_general.js
+++ template/33-Product-Reviewer-Amazon-USA/amazon-product-reviewer-usa/js/script_general.js
@@ -22,23 +22,13 @@ var thx_time = 2500
 window.standart_time = standart_time
 window.thx_time = thx_time
 
-$("header a").click(function(e){
+$(".btn-to-forn").click(function(e){
     e.preventDefault();
-    var full_url = this.href;
-    var parts = full_url.split("#");
-    var trgt = parts[1];
-    var target_offset = $("#"+trgt).offset();
-    var target_top = target_offset.top;
-    $('html, body').animate({scrollTop:target_top}, 1500);
-});
-
-$(".start-subscribe a").click(function(e){
-    e.preventDefault();
-    var full_url = this.href;
-    var parts = full_url.split("#");
-    var trgt = parts[1];
-    var target_offset = $("#"+trgt).offset();
-    var target_top = target_offset.top;
+    let full_url = "#intro";
+    let parts = full_url.split("#");
+    let trgt = parts[1];
+    let target_offset = $("#"+trgt).offset();
+    let target_top = target_offset.top;
     $('html, body').animate({scrollTop:target_top}, 1500);
 })
 
