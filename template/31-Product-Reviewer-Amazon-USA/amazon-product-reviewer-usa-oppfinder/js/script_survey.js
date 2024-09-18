dacd63e29dc5e75614799e904671e7ca34d1ad87
Pavlo Svystun <nefely123@gmail.com>
2024-09-18 13:31:48 +0300

    31

--- template/31-Product-Reviewer-Amazon-USA/amazon-product-reviewer-usa-oppfinder/js/script_survey.js
+++ template/31-Product-Reviewer-Amazon-USA/amazon-product-reviewer-usa-oppfinder/js/script_survey.js
@@ -9,8 +9,34 @@ const sub14 = window.getURLParameter(window.location.href, 'sub14');
 const sub15 = window.getURLParameter(window.location.href, 'sub15');
 const sub16 = window.getURLParameter(window.location.href, 'sub16');
 
+const offer_type = window.getURLParameter(window.location.href, 'offer_type');
+
 const rtkClickID__ = window.getURLParameter(window.location.href, 'clickid');
 const cachebuster__ = window.getURLParameter(window.location.href, 'rtkck');
+
+
+const data = {
+    "zip": sub12, 
+    "firstname": sub13, 
+    "lastname": sub14, 
+    "email": sub15, 
+    "phone": sub16, 
+    "offer_type": offer_type, 
+    "offer_url": window.location.href.split('?')[0].replace("/survey/" , "").replace("/survey" , ""), 
+    "click_id": rtkClickID__
+};
+console.log(data)
+// uncommit on prod
+fetch(`https://track.oppcenter.net/postback?type=CompleteRegistration&clickid=${rtkClickID__}`, { mode: 'no-cors'})
+.then(r => {
+    console.log("successfully registered: " + rtkClickID__);
+    fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
+    .then(rr => {
+        console.log("successfully registered lead in Data API: " + rtkClickID__)
+    })
+    .catch(ed => {});
+})
+.catch(e => console.log("error during registration lead: " + e));
 
 setTimeout(()=> {
     $("#intro .thx").fadeOut(300)
