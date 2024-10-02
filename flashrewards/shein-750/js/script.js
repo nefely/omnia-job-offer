a6b0732041dab830231f7e3b7426ff721e3e5b1d
Pavlo Svystun <nefely123@gmail.com>
2024-10-02 14:30:02 +0300

    +

---
+++ flashrewards/shein-750/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#feature-inner").click(function(){
+        $("#feature-wrapper").addClass("on")
+        $("#feature").css("opacity" , "1");
+    })
+
+    $("#exit_icon").click(function(e){
+        e.stopPropagation();
+        $("#feature-wrapper").removeClass("on")
+    })
+
+    $("#feature-inner .pr-info-btn").click(function(e){
+        e.stopPropagation();
+        $("#feature-wrapper").removeClass("on")
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    })
+
+})
