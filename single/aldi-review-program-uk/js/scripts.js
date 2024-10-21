4e1027e7c38395ff7639e6532f05cdde5c34b2d7
Pavlo Svystun <nefely123@gmail.com>
2024-10-21 16:39:31 +0300

    -++++

--- single/aldi-review-program-uk/js/scripts.js
+++ single/aldi-review-program-uk/js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.preloader').delay(300).fadeOut(300);
 
-    $("input[name=email]").val("")
-
     window.domain = window.location.hostname;
     $(".domain").text(domain)
     $(".mailto").attr("href" , `mailto:contact@${domain}`)
@@ -38,73 +36,27 @@ $(document).ready(function(){
         },300)
     })
 
-    // spinner
-    $(".spinner .btn").on("click" , function(){
-        $(".spinner").fadeOut(300)
-        setTimeout(function(){
-            $(".form").fadeIn(300)
-            $(".container").removeClass("bg")
-        },300)
-    })
-
-    // form
-    let offer_link = "";
-    let final_link = "";
-
-    setTimeout(() => {
-        offer_link = $('#offer').attr("href");
-    }, 2000);
-
-    $(".email").on("input", function() {
-        final_link = `${offer_link}${offer_link.includes("?") ? "&" : "?"}sub15=${$("[name=email]").val()}`;
-        $("#offer").attr("href", final_link);
-    });
-
-    $("input[name=email]").on("input", function() {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
-            $("input[name=email]").removeClass("error");
-        }
-    });
-
     let clickAllowed = true;
 
     $("#offer").on("click", function(e) {
+        e.preventDefault(); 
+
         if (!clickAllowed) {
-            e.preventDefault(); 
             return;
         }
-    
-        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/.test($("input[name=email]").val())) {
-            e.preventDefault();
-            $("input[name=email]").addClass("error");
-            return;
-        } else {
-            e.preventDefault()
-            fetch(`https://omniapostback.com/postback?status=other&type=CompleteRegistration&clickid=${rtkClickID}&sum=0`, { mode: 'no-cors'})
-            .then(r => {
-                console.log("successfully registered: " + rtkClickID);
-                setTimeout(()=>{
-                    window.location.href = $(this).attr('href');
-                }, 500)
-            })
-            .catch(e => console.log("error during registration lead: " + e));
-        }
+
+        fetch(`https://omniapostback.com/postback?status=other&type=CompleteRegistration&clickid=${rtkClickID}&sum=0`, { mode: 'no-cors'})
+        .then(r => {
+            console.log("successfully registered: " + rtkClickID);
+            setTimeout(()=>{
+                window.location.href = $(this).attr('href');
+            }, 500)
+        })
+        .catch(e => console.log("error during registration lead: " + e));
     
         clickAllowed = false;
         setTimeout(() => {
             clickAllowed = true; 
         }, 5000);
     });
-
-
-// test
-// $(".quiz").fadeOut(0)
-// $(".spinner").fadeOut(0)
-// $(".form").fadeIn(0)
-// test
-
-
-
-    
-
 })
