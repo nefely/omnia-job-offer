7dd955606e32a7fde928f995a13cae527802a3b3
Pavlo Svystun <nefely123@gmail.com>
2024-11-18 11:59:58 +0200

    +

--- prod/hustleside.pro/amzn-pr-tt/js/script_survey.js
+++ prod/hustleside.pro/amzn-pr-tt/js/script_survey.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 const clickid = window.getURLParameter(window.location.href, 'clickid');
 const uclick = window.getURLParameter(window.location.href, 'uclick');
 
-const sub12 = window.getURLParameter(window.location.href, 'sub12');
 const sub13 = window.getURLParameter(window.location.href, 'sub13');
 const sub14 = window.getURLParameter(window.location.href, 'sub14');
 const sub15 = window.getURLParameter(window.location.href, 'sub15');
@@ -14,8 +13,9 @@ const offer_type = window.getURLParameter(window.location.href, 'offer_type');
 const rtkClickID__ = window.getURLParameter(window.location.href, 'clickid');
 const cachebuster__ = window.getURLParameter(window.location.href, 'rtkck');
 
+
 const data = {
-    "zip": sub12, 
+    "zip": "",
     "firstname": sub13, 
     "lastname": sub14, 
     "email": sub15, 
@@ -24,38 +24,40 @@ const data = {
     "offer_url": window.location.href.split('?')[0].replace("/survey/" , "").replace("/survey" , ""), 
     "click_id": rtkClickID__
 };
-
-if (localStorage.getItem(window.location.href.split("?")[0]) !== window.getURLParameter(window.location.href, 'clickid')) {
-    fetch(`https://track.earnoppcenter.net/postback?type=CompleteRegistration&clickid=${rtkClickID__}`, { mode: 'no-cors'})
-    .then(r => {
-        console.log("successfully registered: " + rtkClickID__);
-        fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
-        .then(rr => {
-            console.log("successfully registered lead in Data API: " + rtkClickID__)
-            localStorage.setItem(window.location.href.split("?")[0] , window.getURLParameter(window.location.href, 'clickid'))
-        })
-        .catch(ed => {});
+console.log(data)
+// uncommit on prod
+fetch(`https://track.hustleside.pro/postback?type=CompleteRegistration&clickid=${rtkClickID__}`, { mode: 'no-cors'})
+.then(r => {
+    console.log("successfully registered: " + rtkClickID__);
+    fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
+    .then(rr => {
+        console.log("successfully registered lead in Data API: " + rtkClickID__)
     })
-    .catch(e => console.log("error during registration lead: " + e));
-    
-}
-
+    .catch(ed => {});
+})
+.catch(e => console.log("error during registration lead: " + e));
 
 setTimeout(()=> {
     $("#intro .thx").fadeOut(300)
     $("#intro .quiz").delay(300).fadeIn(300)
+    $("body").css("background" , "#F3F4F6");
 }, 3000)
 
+
+$('.quiz-dots .circle').removeClass("active")
+$('.quiz-dots .circle:eq(0)').addClass("active")
+
 form_final_link = () => {
-    $(".quiz-block--4 a.yes").attr("href" ,`${window.offer_link_1}${window.offer_link_1.includes("?") ? "&" : "?"}clickid=${rtkClickID__}&rtkck=${cachebuster__}&sub12=${sub12}&sub13=${sub13}&sub14=${sub14}&sub15=${sub15}&sub16=${sub16}` )
+    $(".quiz-block--4 a.yes").attr("href" ,`${window.offer_link_1}${window.offer_link_1.includes("?") ? "&" : "?"}clickid=${rtkClickID__}&rtkck=${cachebuster__}&sub13=${sub13}&sub14=${sub14}&sub15=${sub15}&sub16=${sub16}` )
 }
 	
 // quiz flow
 $(".quiz-block--1 .quiz-block-answers a").click(function(e){
     e.preventDefault()
 
-    $('.quiz-dots ul li').removeClass("active")
-    $('.quiz-dots ul li:eq(1)').addClass("active")
+    $('.quiz-dots .circle').removeClass("active")
+    $('.quiz-dots .circle:eq(1)').addClass("active")
+    $('.quiz-dots .circle:eq(0)').addClass("passed")
 
     $(this).closest(".quiz-block").fadeOut(standart_time)
     setTimeout(()=>{
@@ -65,8 +67,9 @@ $(".quiz-block--1 .quiz-block-answers a").click(function(e){
 $(".quiz-block--2 .quiz-block-answers a").click(function(e){
     e.preventDefault()
 
-    $('.quiz-dots ul li').removeClass("active")
-    $('.quiz-dots ul li:eq(2)').addClass("active")
+    $('.quiz-dots .circle').removeClass("active")
+    $('.quiz-dots .circle:eq(2)').addClass("active")
+    $('.quiz-dots .circle:eq(1)').addClass("passed")
 
     $(this).closest(".quiz-block").fadeOut(standart_time)
     setTimeout(()=>{
@@ -77,19 +80,21 @@ $(".quiz-block--3 .quiz-block-answers a").click(function(e){
     e.preventDefault()
     $(this).closest(".quiz-block").css("opacity" , '0')
 
+    $('.quiz-dots .circle:eq(2)').addClass("passed")
+
     window.offer_link_1 = $('.quiz-block--4 a.yes').attr("href")
 
     form_final_link()
 
     console.log("hide")
 
-    $('.quiz-dots ul li').removeClass("active")
+     $('.quiz-dots .circle').removeClass("active")
     setTimeout(()=>{
-        $('.quiz-dots ul li:eq(0)').addClass("green")
+        $('.quiz-dots .circle:eq(0)').addClass("active")
         setTimeout(()=>{
-            $('.quiz-dots ul li:eq(1)').addClass("green")
+            $('.quiz-dots .circle:eq(1)').addClass("active")
             setTimeout(()=>{
-                $('.quiz-dots ul li:eq(2)').addClass("green")
+                $('.quiz-dots .circle:eq(2)').addClass("active")
                 setTimeout(()=>{
                     $('.quiz-dots').fadeOut(300)
                     setTimeout(()=>{
@@ -132,17 +137,20 @@ $(".quiz-block .quiz-block-back button").click(function(){
 
 
 $(".quiz-block--2 .quiz-block-back button").click(function(){
-    $('.quiz-dots ul li').removeClass("active")
-    $('.quiz-dots ul li:eq(0)').addClass("active")
+    $('.quiz-dots .circle').removeClass("active").removeClass("passed")
+    $('.quiz-dots .circle:eq(0)').addClass("active")
 })
 $(".quiz-block--3 .quiz-block-back button").click(function(){
-    $('.quiz-dots ul li').removeClass("active")
-    $('.quiz-dots ul li:eq(1)').addClass("active")
+    $('.quiz-dots .circle').removeClass("active").removeClass("passed")
+    $('.quiz-dots .circle:eq(0)').addClass("passed")
+    $('.quiz-dots .circle:eq(1)').addClass("active")
 })
 $(".quiz-block--4 .quiz-block-back button").click(function(){
     $('.quiz-dots').fadeIn(300)
-    $('.quiz-dots ul li').removeClass("active").removeClass("green")
-    $('.quiz-dots ul li:eq(2)').addClass("active")
+    $('.quiz-dots .circle').removeClass("active").removeClass("passed")
+    $('.quiz-dots .circle:eq(0)').addClass("passed")
+    $('.quiz-dots .circle:eq(1)').addClass("passed")
+    $('.quiz-dots .circle:eq(2)').addClass("active")
 })
 
 })
