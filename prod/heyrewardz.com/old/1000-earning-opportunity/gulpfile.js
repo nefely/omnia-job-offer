efea3fe98fd7dd87332088b1a16bf06b59148dbb
Pavlo Svystun <nefely123@gmail.com>
2024-12-17 13:07:58 +0200

    prod

--- prod/heyrewardz.com/old/1000-earning-opportunity/gulpfile.js
+++ prod/heyrewardz.com/old/1000-earning-opportunity/gulpfile.js
@@ -8,14 +8,13 @@ gulp.task('serve', function() {
       baseDir: "./"
     },
     online: true,
-    // tunnel: true,
-    // tunnel: "nefely",
-    // logLevel: "debug"
+    tunnel: true,
+    tunnel: "nefely",
+    logLevel: "debug"
   });
 
   gulp.watch("./**/*.html").on("change", reload);
   gulp.watch("./**/*.css").on("change", reload);
   gulp.watch("./**/*.js").on("change", reload);
 
-  /*http://localhost:3000/*/ 
 });
