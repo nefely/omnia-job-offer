792b14050ab2d00f56a4f71d7a1524f9b1ee0e18
Pavlo Svystun <nefely123@gmail.com>
2025-01-30 14:31:39 +0200

    prods + template

--- template/46-Product-Reviewer-Amazon/product-review-amazon-v2/js/survey.js
+++ template/46-Product-Reviewer-Amazon/product-review-amazon-v2/js/survey.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         "click_id": rtkClickID__
     };
 
-    fetch(`https://track.brandsspot.net/postback?type=CompleteRegistration&clickid=${rtkClickID__}`, { mode: 'no-cors'})
+    fetch(`https://omniapostback.com/postback?type=CompleteRegistration&clickid=${rtkClickID__}`, { mode: 'no-cors'})
     .then(r => {
         console.log("successfully registered: " + rtkClickID__);
         fetch("https://data.omniatrackroi.com/api/leads", { method: "POST", mode: "no-cors", body: JSON.stringify(data) })
