e64850a89f19b5ccfbe537a91a65d239bbd6c068
Pavlo Svystun <nefely123@gmail.com>
2025-06-17 14:14:09 +0300

    ++

---
+++ prod/rewardyplace.space/car-ins/js/global.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	
+    window.domain = window.location.hostname;
+    $(".domain").text(domain)
+    
+    $(".mailto").attr("href" , `mailto:contact@${domain}`)
+    
+    window.getURLParameter = (sUrl, sParam) => {
+        let sPageURL = decodeURI(sUrl.substring(sUrl.indexOf('?') + 1));
+        let sURLVariables = sPageURL.split('&');
+        for (let i = 0; i < sURLVariables.length; i++) {
+            let sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+
+    $(".offer_link").click(function(e){
+        e.preventDefault();
+        
+        ttq.track('CompleteRegistration');
+
+        fetch(`https://track.${window.location.host}/postback?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
+        .then(r => {
+            console.log("successfully registered: " + rtkClickID);
+        })
+        .catch(e => console.log("error during registration lead: " + e));
+
+        setTimeout(()=>{
+            window.location.href = $(this).attr("href")
+        },500)
+    })
+    
+    
+})    
