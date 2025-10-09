a02fc5766342c91bce312427e987cacf825b7518
Pavlo Svystun <nefely123@gmail.com>
2025-10-09 17:51:18 +0300

    ++

--- prod/globalrewardz.com/js/scripts.js
+++ prod/globalrewardz.com/js/scripts.js
@@ -17,9 +17,9 @@ $(document).ready(function(){
 // $(".offer_link").click(function(e){
 //     e.preventDefault()
 //     $(this).addClass("disabled")
-//     fetch(`https://track.${window.location.host}/postback?type=CompleteRegistration&clickid=${rtkcid}`, { mode: 'no-cors'})
+//     fetch(`https://track.${window.location.host}/postback?type=CompleteRegistration&clickid=${rtkClickID}`, { mode: 'no-cors'})
 //     .then(r => {
-//         console.log("successfully registered: " + rtkcid);
+//         console.log("successfully registered: " + rtkClickID);
 //         setTimeout(()=>{
 //             window.location.href = $(this).attr("href")
 //         },300)
