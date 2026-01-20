9e838938c29b9537ea3be2fa241e22f12d7afeda
Pavlo Svystun <nefely123@gmail.com>
2026-01-20 13:59:37 +0200

    templates

---
+++ parse/playful-rewards-mini-v4-ca/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	
+window.domain = window.location.hostname;
+$(".domain").text(domain)
+
+$(".mailto").attr("href" , `mailto:contact@${domain}`)
+
+window.getURLParameter = (sUrl, sParam) => {
+    let sPageURL = decodeURI(sUrl.substring(sUrl.indexOf('?') + 1));
+    let sURLVariables = sPageURL.split('&');
+    for (let i = 0; i < sURLVariables.length; i++) {
+        let sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}   
+
+$('[data-over]').each(function () {
+  const $el = $(this);
+  const target = parseInt($el.data('over'), 10);
+
+  $({ count: 0 }).animate(
+    { count: target },
+    {
+      duration: 3000,
+      easing: 'swing',
+      step: function (now) {
+        $el.text(Math.floor(now));
+      },
+      complete: function () {
+        $el.text(target);
+      }
+    }
+  );
+});
+
+
+
+
+const rtkcid = window.getURLParameter(window.location.href, 'rtkcid');
+const rtkcmpid = window.getURLParameter(window.location.href, 'rtkcmpid');
+
+isClicked = false;
+
+$(".offer_link").click(function(e){
+    e.preventDefault();
+
+    if (isClicked) return;
+    isClicked = true;
+
+    fbq('track', 'PageView');
+
+    setTimeout(() => {
+        window.location.href = $(this).attr("href");
+    }, 500);
+
+    setTimeout(() => {
+        isClicked = false;
+    }, 5000);
+});
+
+
+
+})
