762d40e4850ff04072f701261b3875c349c2786c
Pavlo Svystun <nefely123@gmail.com>
2026-03-27 17:49:44 +0200

    ++

--- prod/playfulrewardsapp.pro/mini-1-mode-102/js/script.js
+++ prod/playfulrewardsapp.pro/mini-1-mode-102/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 window.domain = window.location.hostname;
 $(".domain").text(domain)
 $(".domainwihoutdot").text(domain.split(".")[0])
-$(".mailto").attr("href" , `mailto:contact@${domain}`)
+$(".mailto").attr("href" , `mailto:contact@${domain}`).text(`contact@${domain}`)
+$(".support").attr("href" , `mailto:support@${domain}`).text(`support@${domain}`)
 })
