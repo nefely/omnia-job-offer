160f1d11674c7addb4866bc75b681218e969a5af
Pavlo Svystun <nefely123@gmail.com>
2026-04-29 15:14:27 +0300

    ++++

---
+++ prod/fluxrewards.net/js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+window.getURLParameter = (sUrl, sParam) => {
+    let sPageURL = decodeURI(sUrl.substring(sUrl.indexOf('?') + 1));
+    let sURLVariables = sPageURL.split('&');
+    for (let i = 0; i < sURLVariables.length; i++) {
+        let sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}   
+
+const rtkcid = window.getURLParameter(window.location.href, 'rtkcid');
+const rtkcmpid = window.getURLParameter(window.location.href, 'rtkcmpid');
+
+isClicked = false;
+
+$(".offer_link").click(function(e){
+    e.preventDefault();
+
+    if (isClicked) return;
+    isClicked = true;
+
+    fbq('track', 'PageView');
+
+    setTimeout(() => {
+        window.location.href = $(this).attr("href");
+    }, 500);
+
+    setTimeout(() => {
+        isClicked = false;
+    }, 5000);
+});
+
+
+})
