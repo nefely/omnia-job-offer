014795c884ebfdc557cf8cfa29270b86a8ed7e55
Pavlo Svystun <nefely123@gmail.com>
2026-07-29 19:49:26 +0300

    ++

---
+++ parse/gravy-pass-tt-wallet/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Handle question navigation
+    $('[data-question="1"] button').click(function() {  
+        $('[data-question="1"]').hide();
+        $('[data-question="2"]').show();
+    });
+
+    window.domain = window.location.hostname;
+    $(".domain").text(domain)
+
+    isClicked = false;
+
+    $(".offer_link").click(function(e){
+        e.preventDefault();
+
+        if (isClicked) return;
+        isClicked = true;
+
+        fbq('track', 'PageView');
+        ttq.page();
+
+
+        setTimeout(() => {
+            window.location.href = $(this).attr("href");
+        }, 500);
+
+        setTimeout(() => {
+            isClicked = false;
+        }, 5000);
+    });
+});
---
+++ prod/gravypass.net/853snap/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    // Handle question navigation
+    $('[data-question="1"] button').click(function() {  
+        $('[data-question="1"]').hide();
+        $('[data-question="2"]').show();
+    });
+
+    window.domain = window.location.hostname;
+    $(".domain").text(domain)
+
+    isClicked = false;
+
+    $(".offer_link").click(function(e){
+        e.preventDefault();
+
+        if (isClicked) return;
+        isClicked = true;
+
+        snaptr('track', 'VIEW_CONTENT');
+
+
+        setTimeout(() => {
+            window.location.href = $(this).attr("href");
+        }, 500);
+
+        setTimeout(() => {
+            isClicked = false;
+        }, 5000);
+    });
+});
